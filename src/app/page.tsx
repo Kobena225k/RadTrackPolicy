d81cebc548a2c488d2943b7d3f9640ec88d0552e
Kobena225k <lucien.kobena@kognitiv.ci>
2025-01-20 16:42:42 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,90 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+   
+      <h1><strong>Politique de confidentialité de Radtrack By Radiant Assistance security</strong></h1>
+    
+      <p>Radiant Assistance Security exploite le service de suivi GPS Radtrack. Cette page vous informe de nos politiques concernant la collecte, l'utilisation et la divulgation des données personnelles lorsque vous utilisez notre Service.
+      </p> 
+      
+      <p>Nous n'utiliserons ni ne partagerons vos informations avec qui que ce soit, sauf comme décrit dans la présente politique de confidentialité.</p>
+      
+      <p>Nous utilisons vos données personnelles pour fournir et améliorer le service. En utilisant le Service, vous acceptez la collecte et l'utilisation des informations conformément à cette politique. Sauf indication contraire dans la présente politique de confidentialité, les termes utilisés dans la présente politique de confidentialité ont la même signification que dans nos Conditions générales, accessibles à l'adresse <a href="[Lien vers vos conditions générales]">[Lien vers vos conditions générales]</a>.</p>
+      
+      <h2><strong>Collecte et utilisation des informations</strong></h2>
+      
+      <p>Lors de l'utilisation de notre Service, nous pouvons vous demander de nous fournir certaines informations personnellement identifiables qui peuvent être utilisées pour vous contacter ou vous identifier. Les informations personnellement identifiables peuvent inclure, sans s'y limiter :</p>
+      
+      <ul> 
+      <li>Nom</li> 
+      <li>Adresse e-mail</li> 
+      <li>Numéro de téléphone</li> 
+      <li>Localisation des appareils suivis</li>
+      </ul>
+      
+      <h2><strong>Données de localisation</strong></h2>
+      
+      <p>Nous collectons les données de localisation des appareils suivis via le Service. Ces données incluent la latitude, la longitude, la vitesse, la direction et l'horodatage. Ces données sont stockées sur nos serveurs sécurisés et sont accessibles uniquement par vous et les personnes que vous autorisez.</p>
+      
+      <h2><strong>Données du journal</strong></h2>
+      
+      <p>Nous collectons des informations que votre navigateur envoie chaque fois que vous visitez notre Service ("Données du journal"). Ces données du journal peuvent inclure des informations telles que l'adresse IP de votre ordinateur, le type de navigateur, la version du navigateur, les pages de notre service que vous visitez, l'heure et la date de votre visite, le temps passé sur ces pages et d'autres statistiques.</p> 
+      
+      <h2><strong>Cookies</strong></h2>
+      
+      <p>Les cookies sont des fichiers contenant une petite quantité de données, qui peuvent inclure un identifiant unique anonyme. Les cookies sont envoyés à votre navigateur depuis un site Web et stockés sur le disque dur de votre ordinateur.</p>
+      
+      <p>Nous utilisons des cookies pour collecter des informations. Vous pouvez demander à votre navigateur de refuser tous les cookies ou d'indiquer quand un cookie est envoyé. Cependant, si vous n'acceptez pas les cookies, vous ne pourrez peut-être pas utiliser certaines parties de notre Service.</p>
+      
+      <h2><strong>Fournisseurs de services</strong></h2>
+      
+      <p>Nous pouvons faire appel à des sociétés tierces et des particuliers pour faciliter notre Service, pour fournir le Service en notre nom, pour effectuer des services liés au Service ou pour nous aider à analyser l'utilisation de notre Service.</p>
+      
+      <p>Ces tiers n'ont accès à vos informations personnelles que pour effectuer ces tâches en notre nom et sont tenus de ne pas les divulguer ni les utiliser à d'autres fins.</p>
+      
+      <h2><strong>Sécurité</strong></h2> 
+      
+      <p>La sécurité de vos informations personnelles est importante pour nous, mais rappelez-vous qu'aucune méthode de transmission sur Internet ou méthode de stockage électronique n'est sécurisée à 100 %. Bien que nous nous efforcions d'utiliser des moyens commercialement acceptables pour protéger vos informations personnelles, nous ne pouvons pas garantir leur sécurité absolue.</p>
+      
+      <h2><strong>Conformité à la loi</strong></h2>
+      
+      <p>Nous divulguerons vos informations personnelles si la loi l'exige ou si nous pensons de bonne foi qu'une telle action est nécessaire pour :</p> 
+      
+      <ul> 
+      <li>Se conformer à une obligation légale</li> 
+      <li>Protéger et défendre les droits ou la propriété de Radiant Assistance Security</li> 
+      <li>Prévenir ou enquêter sur d'éventuels actes répréhensibles liés au Service</li> 
+      <li>Protéger la sécurité personnelle des utilisateurs du Service ou du public</li> 
+      <li>Se protéger contre la responsabilité légale</li> 
+      </ul>
+      
+      <h2><strong>Transfert international de données</strong></h2>
+      
+      <p>Vos informations, y compris les données personnelles, peuvent être transférées vers des ordinateurs situés en dehors de votre état, province, pays ou autre juridiction gouvernementale où les lois sur la protection des données peuvent différer de celles de votre juridiction.</p>
+      
+      <p>Si vous vous trouvez en dehors Côte d'ivoire et choisissez de nous fournir des informations, veuillez noter que nous transférons les informations, y compris les données personnelles, vers Côte d'ivoire et les traitons là-bas.</p>
+      
+      <p>Votre consentement à la présente politique de confidentialité suivi de votre soumission de telles informations représente votre accord à ce transfert.</p>
+      
+      <h2><strong>Liens vers d'autres sites</strong></h2>
+      
+      <p>Notre Service peut contenir des liens vers d'autres sites que nous n'exploitons pas. Si vous cliquez sur un lien tiers, vous serez redirigé vers le site de ce tiers. Nous vous conseillons vivement de consulter la politique de confidentialité de chaque site que vous visitez.</p>
+      
+      <p>Nous n'avons aucun contrôle sur le contenu, les politiques de confidentialité ou les pratiques des sites ou services tiers et n'en assumons aucune responsabilité.</p>
+      
+      <h2><strong>Confidentialité des enfants</strong></h2>
+      
+      <p>Notre Service ne s'adresse pas aux personnes de moins de 13 ans ("Enfants"). Nous ne collectons pas sciemment d'informations personnellement identifiables auprès d'enfants de moins de 13 ans. Si vous êtes un parent ou un tuteur et que vous savez que votre enfant nous a fourni des informations personnelles, veuillez nous contacter. Si nous découvrons qu'un enfant de moins de 13 ans nous a fourni des informations personnelles, nous supprimerons ces informations de nos serveurs immédiatement.</p>
+      
+      <h2><strong>Modifications de cette politique de confidentialité</strong></h2>
+      <p>Nous pouvons mettre à jour notre politique de confidentialité de temps à autre. Nous vous informerons de tout changement en publiant la nouvelle politique de confidentialité sur cette page.</p>
+      
+      <p>Il est conseillé de consulter régulièrement cette politique de confidentialité pour tout changement. Les modifications de cette politique de confidentialité entrent en vigueur lorsqu'elles sont publiées sur cette page.</p>
+      
+      <h2><strong>Contactez-nous</strong></h2>
+      
+      <p>Si vous avez des questions concernant cette politique de confidentialité, veuillez nous contacter à l'adresse <a href="mailto:[service.technique@radiant.ci]">service.technique@radiant.ci</a>.</p>
     </div>
   );
 }
